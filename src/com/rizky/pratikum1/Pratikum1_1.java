bd8e152fc41baaef429cdd66819614b4bb232d6b
rizkyhaksono <mrizkyhaksono@gmail.com>
2022-10-04 00:55:58 +0700

    modul 2 tugas 2 fix

--- src/com/rizky/pratikum1/Pratikum1_1.java
+++ src/com/rizky/pratikum1/Pratikum1_1.java
@@ -8,14 +8,25 @@ public class Pratikum1_1 {
 
     public static void main(String[] args) {
 
+        int[] arr = new int[5];
+
         try {
-            int[] arr = new int[5];
+            // 0 1 2 3 4
             arr[5] = 25;
+            System.out.println(arr[5]);
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
 
         // index 5 out of bounds for length 5
+
+//        try {
+//            int[] arr = new int[6];
+//            arr[5] = 25;
+//            System.out.println(arr[5]);
+//        } catch (Exception e) {
+//            System.out.println("Error: " + e);
+//        }
 
     }
 }
--- src/com/rizky/pratikum2/Pratikum2_2.java
+++ src/com/rizky/pratikum2/Pratikum2_2.java
@@ -4,73 +4,110 @@
 
 package com.rizky.pratikum2;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Pratikum2_2 {
 
-    abstract static class cekDate {};
-    abstract class cekFrom {};
-    abstract class cekName {};
-    abstract class list {};
+    public String[] book = new String[1];
+    public String[] place = new String[1];
+    public int[] date = new int[1];
 
-    static String[][] book = {{"This is Book, Malang.2022"}};
-    static String[][] place = {{"Malang"}, {"Bandung"}, {"Surabaya"}};
-
-    static void cekDate(int date) {
+    void cekDate(int date) {
         if (date < 2018 || date > 2022) {
-
+            System.out.println("Tahun tidak valid");
+            System.out.println("Masukkan date antara tahun 2018 - 2022");
+        } else {
+            System.out.println("Tahun valid");
         }
     }
 
-    static void cekFrom(String from) {
-        for (int i = 0; place[i][0] != null; i++) {
-            if (from.compareTo(Arrays.toString(place[i])) == 0) {
+    void cekFrom(String from) {
+        for (int i = 0; from.equals(place[i]); i++) {
+            if (from.compareTo(place[i]) == 0) {
                 return;
             }
         }
-
     }
 
-    static void cekName(String name) {
-        for (int i = 0; book[i][0] != null; i++) {
-            if (name.compareTo(Arrays.toString(book[i])) == 0) {
+    void cekName(String name) {
+        for (int i = 0; name.equals(book[i]); i++) {
+            if (name.compareTo(book[i]) == 0) {
                 return;
             }
         }
     }
 
     void list() {
-        for (int i = 0; book[i][0] != null; i++) {
-            System.out.println(Arrays.toString(book[i]));
+        for (int i = 0; i < book.length && i < place.length && i < date.length; i++) {
+            System.out.println("[" + i + "] " + book[i] + ", " + place[i] + ". " + date[i]);
         }
     }
 
     public static void main(String[] args) {
-        String inName, inFrom, inDate;
-        int inIndex = 0;
+
+        Pratikum2_2 pTwo = new Pratikum2_2();
+
+        String inName, inFrom;
+        int inDate;
+
+        int num;
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Welcome to Library");
+        try {
+            System.out.println("Welcome to Library");
 
-        System.out.println("Nama : ");
-        inName = input.nextLine();
-        cekName(String.valueOf(inName));
+            System.out.print("Nama : ");
+            inName = input.nextLine();
+            pTwo.cekName(inName);
 
-        System.out.println("From : ");
-        inFrom = input.nextLine();
-        cekFrom(String.valueOf(inFrom));
+            System.out.print("From : ");
+            inFrom = input.nextLine();
+            pTwo.cekFrom(inFrom);
 
-        System.out.println("Date : ");
-        inDate = input.nextLine();
-        cekDate(Integer.parseInt(inDate));
+            System.out.print("Date : ");
+            inDate = input.nextInt();
+            pTwo.cekDate(inDate);
 
-        System.out.println(inName + "" + inFrom + "" + inDate);
-        book[1] = new String[inIndex];
+            do {
+                if (inDate < 2018 || inDate > 2022) {
+                    System.out.print("Date : ");
+                    inDate = input.nextInt();
+                    pTwo.cekDate(inDate);
+                    num = -1;
+                } else {
+                    num = 1;
+                }
+            } while (num == -1);
 
-        System.out.println("Buku berhasil ditambahan");
+            String bookName = inName;
+            String placeName = inFrom;
+            int dateName = inDate;
+
+            for (int i = 0; i < pTwo.book.length && i < pTwo.place.length && i < pTwo.date.length; i++) {
+                pTwo.book[i] = bookName;
+                pTwo.place[i] = placeName;
+                pTwo.date[i] = dateName;
+            }
+
+            System.out.println("Buku berhasil ditambahan");
+            pTwo.list();
+
+            // ask to add more
+            System.out.print("\nTambah buku lagi? [y/n] ");
+            String ask = input.next();
+            if (ask.equals("y")) {
+                main(args);
+            } else {
+                System.exit(0);
+                System.out.println("Terima kasih");
+            }
+
+        } catch (Exception e) {
+            System.out.println("Buku gagal ditambahan");
+            System.out.println("Error : " + e);
+            main(args);
+        }
+
     }
-
-
 }
