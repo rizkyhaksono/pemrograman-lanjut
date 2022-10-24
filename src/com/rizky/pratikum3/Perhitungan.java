65e7df882a6575ef4d65186fb8e865f14438d13b
rizkyhaksono <mrizkyhaksono@gmail.com>
2022-10-24 07:35:00 +0700

    feat: perhitungan modul 3

--- src/com/rizky/pratikum3/Perhitungan.java
+++ src/com/rizky/pratikum3/Perhitungan.java
@@ -13,7 +13,17 @@ public class Perhitungan {
         return 4 * (sisi.getPanjang() * sisi.getLebar() * sisi.getTinggi());
     };
 
+//    static boolean isCube(long input) {
+//        return Math.round(Math.cbrt(input)) *
+//                Math.round(Math.cbrt(input)) *
+//                Math.round(Math.cbrt(input)) == input;
+//    }
+
     static boolean isCube(long input) {
+        return extractMethod(input);
+    }
+
+    private static boolean extractMethod(long input) {
         return Math.round(Math.cbrt(input)) *
                 Math.round(Math.cbrt(input)) *
                 Math.round(Math.cbrt(input)) == input;
