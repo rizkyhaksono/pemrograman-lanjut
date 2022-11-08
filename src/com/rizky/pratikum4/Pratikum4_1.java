1832ced05751bd4fca4c5f8662b4db71eabc8aa4
rizkyhaksono <mrizkyhaksono@gmail.com>
2022-11-08 15:28:09 +0700

    add: new instruction in comment

--- src/com/rizky/pratikum4/Pratikum4_1.java
+++ src/com/rizky/pratikum4/Pratikum4_1.java
@@ -7,6 +7,7 @@ package com.rizky.pratikum4;
 public class Pratikum4_1 {
 
     public static void main(String[] args) {
+        // open setting -> live template -> java -> print / println / method
         // shortcut for intellij idea is + tab
 
         // print
