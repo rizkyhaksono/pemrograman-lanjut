d00c11c3acc62bd826ec66b8ad3572c9b5710a6f
rizkyhaksono <mrizkyhaksono@gmail.com>
2022-11-28 08:08:08 +0700

    fix: modul 5 kegiatan 2

--- src/com/rizky/pratikum5/Pratikum5_1.java
+++ src/com/rizky/pratikum5/Pratikum5_1.java
@@ -4,33 +4,40 @@
 
 package com.rizky.pratikum5;
 
-import java.util.Arrays;
-import java.util.Scanner;
+// java library
 
 public class Pratikum5_1 {
 
     public static void main(String[] args) {
 
-        Scanner scn = new Scanner(System.in);
+            int[] data = {-75, -10, 0, 30, 0, -30, 90, 70, 89, -50, -55, -30};
+            int temp;
 
-        int[] data = {-75, -10, 0, 30, 0, -30, 90, 70, 89, -50, -55, -30};
-        int temp;
+            System.out.println("Data sebelum diurutkan : ");
+            for (int num : data) {
+                System.out.print(num + " | ");
+            }
 
-        System.out.println("Data sebelum diurutkan : ");
-        for (int num : data) {
-            System.out.print(num + " | ");
-        }
+            // sort array
+            for (int i = 0; i < data.length; i++) {
+                for (int j = i + 1; j < data.length; j++) {
+                    if (data[i] > data[j]) {
+                        temp = data[i];
+                        data[i] = data[j];
+                        data[j] = temp;
+                    }
+                }
+            }
 
-        Arrays.sort(data);
-        System.out.println("\n\nData setelah diurutkan : ");
-        for (int num : data) {
-            System.out.print(num + " | ");
-        }
+            System.out.println("\n\nData setelah diurutkan : ");
+            for (int num : data) {
+                System.out.print(num + " | ");
+            }
 
-        // search index of array
-        System.out.print("\n\nMasukkan index elemen yang mau dicari : ");
-        temp = scn.nextInt();
+            // search index of array
+            System.out.print("\n\nMasukkan index elemen yang mau dicari : ");
+            temp = new java.util.Scanner(System.in).nextInt();
 
-        System.out.println("Nilai elemen ke-" + temp + " adalah " + data[temp]);
+            System.out.println("Nilai elemen ke-" + temp + " adalah " + data[temp]);
     }
 }
